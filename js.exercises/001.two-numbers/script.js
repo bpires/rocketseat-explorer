16bf5439099b409acc9457bdc8f05575136f3642
Rafael Barros <86871991+bpires@users.noreply.github.com>
2022-06-11 18:51:46 -0300

    add js.exercise #001

---
+++ js.exercises/001.two-numbers/script.js
@@ -0,0 +1,20 @@
+/* Soma de dois números */
+
+let num1 = prompt("Qual o primeiro número?")
+
+let num2 = prompt("Qual o segundo número?")
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+const sum = num1 + num2
+const sub = num1 - num2
+const mult = num1 * num2
+const div = num1 / num2
+const restDiv = num1 % num2
+
+alert('A soma resulta ' + sum)
+alert('A subtração resulta ' + sub)
+alert('A multiplicação resulta ' + mult)
+alert('A divisão resulta ' + div)
+alert('O resto da divisão é ' + restDiv)
