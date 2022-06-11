6bbe83102200fc1a9a5aee50c631019cf5069a28
Rafael Barros <86871991+bpires@users.noreply.github.com>
2022-06-11 19:38:32 -0300

    Fix exercise description

--- js.exercises/001.two-numbers/script.js
+++ js.exercises/001.two-numbers/script.js
@@ -1,4 +1,12 @@
-/* Soma de dois números */
+/*
+  Capturar 2 números
+  e fazer as operações matemáticas
+  de soma, subtração, multiplicação,
+  divisão e resto da divisão.
+
+  E para cada operação, mostrar um alerta
+  com o resultado.
+*/
 
 let num1 = prompt("Qual o primeiro número?")
 
