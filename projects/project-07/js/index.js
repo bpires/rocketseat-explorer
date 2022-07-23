15e723ff26a290c8e4c83175cb886b7882af4826
Rafael B. Pires <86871991+bpires@users.noreply.github.com>
2022-07-23 11:17:02 -0300

    restructure js files

--- projects/project-07/js/index.js
+++ projects/project-07/js/index.js
@@ -1,8 +1,7 @@
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
 const buttonStop = document.querySelector('.stop')
-const buttonSet = document.querySelector('.stopwatch')
-
+const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
 
